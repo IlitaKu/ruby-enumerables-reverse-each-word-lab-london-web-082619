a31f2a4adaebf7ec5b4630786b5741faf5bdf56f
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-07-11 12:00:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,13 @@
-#def reverse_each_word(sentence)
-#array_div = sentence.split(" ")
+def reverse_each_word(sentence)
+array_div = sentence.split(" ")
  new_reversed = []
  new_reversed = array_div.collect { |el| el.reverse}.join (" ")
-#end
+end
+
+
+
+
+
 def reverse_each_word(sentence)
 array_diveded = sentence.split(" ")
  new_reversed = []
